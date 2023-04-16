0f434181a7e6d04438cab216bb789a566ba573a9
sanjay <kumarsa2@cisco.com>
2023-04-16 18:38:52 +0530

    modified pom.xml with comments

--- 04-spring-boot-rest-crud/04-springboot-rest-crud-employee with-spring-data-rest/src/main/java/com/luv2code/springboot/cruddemo/DAO/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/04-springboot-rest-crud-employee with-spring-data-rest/src/main/java/com/luv2code/springboot/cruddemo/DAO/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.luv2code.springboot.cruddemo.DAO;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path = "members") we can add like this for prefix
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // no need to write any code because  spring JPA implemented automatically
 }
